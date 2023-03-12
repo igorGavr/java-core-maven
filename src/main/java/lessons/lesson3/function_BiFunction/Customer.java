e2d86a0453547cdd6982d0b66407fea2744ed75d
igorGavr <ragamuffinrogi@gmail.com>
2023-03-12 12:19:14 +0200

    bi_function

---
+++ src/main/java/lessons/lesson3/function_BiFunction/Customer.java
@@ -0,0 +1,11 @@
+package lessons.lesson3.function_BiFunction;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Customer {
+    private String login;
+    private String password;
+}
---
+++ src/main/java/lessons/lesson3/function_BiFunction/User.java
@@ -0,0 +1,10 @@
+package lessons.lesson3.function_BiFunction;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class User {
+    private String name;
+}
