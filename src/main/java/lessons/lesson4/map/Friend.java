39239c9a7e9a928b9fc1197114911929a4dfb1ff
igorGavr <ragamuffinrogi@gmail.com>
2023-03-14 12:57:22 +0200

    map entry

---
+++ src/main/java/lessons/lesson4/map/Friend.java
@@ -0,0 +1,11 @@
+package lessons.lesson4.map;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Friend {
+    private String name;
+    private int age;
+}
---
+++ src/main/java/lessons/lesson4/map/Main.java
@@ -0,0 +1,50 @@
+package lessons.lesson4.map;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) {
+
+        HashMap<String, Friend> map = new HashMap<>();
+        map.put("doctor", new Friend("Stoc", 23));
+        map.put("doctor2", new Friend("Sdf", 93));
+        map.put("doctor3", new Friend("Sas", 24));
+        map.put("doctor4", new Friend("asdftoc", 43));
+
+        System.out.println(map);
+
+        Friend df = map.get("df");
+        System.out.println(df);  // null
+
+        map.containsKey("dfa");
+        map.remove("dfa");
+        map.replace("sdf", new Friend("ad", 23));
+        Set<String> strings = map.keySet();
+        System.out.println(strings);
+
+        Collection<Friend> values = map.values();
+        System.out.println(values);
+        System.out.println("8989898------------90909090");
+
+        Set<Map.Entry<String, Friend>> entrySet = map.entrySet();
+        System.out.println(entrySet);
+        List<Map.Entry<String, Friend>> collect = entrySet.stream().collect(Collectors.toList());
+        System.out.println(collect);
+
+        ArrayList<Map.Entry<String, Friend>> entryArrayList = new ArrayList<>(entrySet);
+
+        Iterator<Map.Entry<String, Friend>> iterator = entrySet.iterator();
+        while (iterator.hasNext()) {
+            Map.Entry<String, Friend> next = iterator.next();
+            System.out.println(next);
+            Friend value = next.getValue();
+            if (value.getAge()%2==0) {
+                iterator.remove();
+            }
+        }
+
+        System.out.println(map);
+
+    }
+}
