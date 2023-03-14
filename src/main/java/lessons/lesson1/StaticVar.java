75495a58041b05db97d1dd4d2f244cc4242ce2dc
igorGavr <ragamuffinrogi@gmail.com>
2023-03-14 19:06:42 +0200

    scope var

---
+++ src/main/java/lessons/lesson1/StaticVar.java
@@ -0,0 +1,24 @@
+package lessons.lesson1;
+
+public class StaticVar {
+    static int a = 10;
+    int b = 23;
+
+    public static void main(String[] args) {
+        int x = 3;
+        System.out.println(x);
+        System.out.println(a);
+        // не статичні зміні видні лише в не статичних класах
+//        System.out.println(b);  // error
+
+
+    }
+    public void checkB() {
+        System.out.println(b);
+    }
+    public static void print() {
+//        System.out.println(b); // error
+        System.out.println(a);
+//        System.out.println(x); // error
+    }
+}
