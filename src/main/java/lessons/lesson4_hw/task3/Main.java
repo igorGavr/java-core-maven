e3e935abe52754abd700b218dd8d6cfe226a194a
igorGavr <ragamuffinrogi@gmail.com>
2023-03-15 15:46:36 +0200

    hw

---
+++ src/main/java/lessons/lesson4_hw/task3/Main.java
@@ -0,0 +1,12 @@
+package lessons.lesson4_hw.task3;
+
+import lessons.lesson4_hw.task3.models.ZooClub;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        ZooClub zooClub = new ZooClub();
+        zooClub.menu();
+    }
+}
---
+++ src/main/java/lessons/lesson4_hw/task3/models/Person.java
@@ -0,0 +1,12 @@
+package lessons.lesson4_hw.task3.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+
+public class Person {
+    private String name;
+
+}
